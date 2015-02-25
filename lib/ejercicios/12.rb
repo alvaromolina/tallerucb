f85f3dfb701b5f7daf7f4468169688a76993bd6b
Alvaro Molina <alvaromolinac@gmail.com>
2015-02-25 11:34:28 -0400

    ultimo

--- lib/ejercicios/12.rb
+++ lib/ejercicios/12.rb
@@ -1,9 +1,30 @@
-# 30 pts for part A
-describe "suma_digitos" do
-  # this is given for free in the outline
-  it "should return 1366 [100 points]" do
-    suma_digitos.should == 1366
+describe "#palindromo?" do
+  it "a) recognizes palindromes correctly [50 points]" do
+    palindromo?("A man a plan a canal Panama").should be_true
+    palindromo?("Madam Im Adam").should be_true
+    palindromo?("Do geese see God").should be_true
+    palindromo?("Murder for a jar of red rum").should be_true
+    palindromo?("Never odd or even").should be_true
+    palindromo?("Racecar").should be_true
+    palindromo?("Was it a car or a cat i saw").should be_true
+  end
+  
+  it "b) recognizes palindromes correctly, with special characters [25 points]" do
+    palindromo?("A man, a plan, a canal -- Panama").should be_true
+    palindromo?("Madam, I'm Adam!").should be_true
+    palindromo?("Do geese see God?").should be_true
+    palindromo?("Murder for a jar of red rum.").should be_true
+    palindromo?("Never odd or & even.").should be_true
+    palindromo?("Racecar").should be_true
+    palindromo?("Was it # a car * or a cat i saw").should be_true
   end
 
-end
 
+  
+  it "recognizes non palindromes correctly [25 points]" do
+    palindromo?("This is not a palindrome").should be_false
+    palindromo?("Trolls, ogres, orcs").should be_false
+    palindromo?("Lorem ipsum dolor sit amet.").should be_false
+    palindromo?("There's some punctuation up in here?!").should be_false
+  end
+end
