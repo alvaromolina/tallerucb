23c7ac56a04851eea015c80f539103e441e5f3c9
Alvaro Molina <alvaromolinac@gmail.com>
2015-03-09 12:08:08 -0400

    examen

---
+++ lib/ejercicios/41.rb
@@ -0,0 +1,8 @@
+describe "programa" do
+
+  it "Debe cargar el programa correctamente [100 points]" do
+
+    
+  end
+
+end
---
+++ lib/ejercicios/42.rb
@@ -0,0 +1,7 @@
+describe "fibonacci_900" do  
+  
+  it "Debe retornar el primer termino de la secuencia fibonnaci que tenga 900 digitos [100 points]" do
+    fibonacci_1000.should == 4304
+  end
+
+end
